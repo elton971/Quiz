7b46d37e241dc5345b7e125b4bae1fcfbf75e3fe
Elton Cavele <ecavele@technoplus.co.mz>
2023-06-16 11:45:40 +0200

    feature(questionCard): completed the business rule for checking the correct answer

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="w-[30rem]">
     <p class="flex text-white text-[2.25rem] font-bold py-5">COUNTRY QUIZ</p>
     <div class="absolute left-[50rem] top-[5rem]">
       <img src="undraw_adventure_4hum 1.svg" alt="" />
@@ -8,29 +8,23 @@
       <h2 class="text-[1rem] text-[#2F527B] font-bold">{{ question }}</h2>
       <div v-for="(option, index) in options" :key="index" class="flex flex-col py-2 mt-5">
         <button
-          :style="{
-            backgroundColor: !selectedOption.result
-              ? selectedOption.value === option
-                ? '#F9A826'
-                : 'white'
-              : selectedOption.value === option && selectedOption.value === correctAnswer
-              ? '#60BF88'
-              : selectedOption.value === option && selectedOption.value !== correctAnswer
-              ? '#EB5757'
-              : option === correctAnswer
-              ? '#60BF88'
-              : 'white',
-            color: !selectedOption.result
-              ? selectedOption.value === option
-                ? 'white'
-                : '#6066D0'
-              : '#6066D0',
-            borderColor: selectedOption.value === option ? '#F9A826' : '#6066D0'
-          }"
+          :style="getOptionStyles(option)"
           @click="handleAnswer(option)"
-          class="border-[0.2rem] border-[#6066D0] rounded-[1rem] items-start p-4 text-left"
+          class="border-[0.2rem] border-[#6066D0] rounded-[1rem] items-start p-4 text-left flex justify-between"
         >
           {{ option }}
+
+          <div v-if="selectedOption.showAnswer">
+            <v-icon
+              :name="
+                selectedOption.value === option && correctAnswer !== option
+                  ? 'fa-regular-window-close'
+                  : selectedOption.value === option && correctAnswer === option
+                  ? 'fa-regular-check-circle'
+                  : correctAnswer === option && 'fa-regular-check-circle'
+              "
+            />
+          </div>
         </button>
       </div>
       <div class="flex justify-end">
@@ -39,8 +33,9 @@
           @click="checkAnswer"
           class="rounded-[1rem] p-4 bg-[#60BF88] text-white"
         >
-          Verificar Resposta
+          Verificar Respostas
         </button>
+
         <button
           v-if="selectedOption.showNextButton"
           @click="nextQuestion"
@@ -59,30 +54,96 @@
 
 <script>
 import { useStore } from 'vuex'
-import { toRaw, reactive } from 'vue'
+import { toRaw, reactive, ref, computed } from 'vue'
+import { OhVueIcon } from 'oh-vue-icons'
 export default {
+  name: 'QuestionCard',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    'v-icon': OhVueIcon
+  },
+  methods: {
+    getOptionStyles(option) {
+      const selectedOption = this.selectedOption.value
+      const correctAnswer = this.correctAnswer
+
+      if (!this.selectedOption.result) {
+        if (selectedOption === option) {
+          return {
+            backgroundColor: '#F9A826',
+            color: 'white',
+            borderColor: '#F9A826'
+          }
+        } else {
+          return {
+            backgroundColor: 'white',
+            color: '#6066D0',
+            borderColor: '#6066D0'
+          }
+        }
+      } else {
+        if (selectedOption === option && selectedOption === correctAnswer) {
+          return {
+            backgroundColor: '#60BF88',
+            color: 'white',
+            borderColor: '#60BF88'
+          }
+        } else if (selectedOption === option && selectedOption !== correctAnswer) {
+          return {
+            backgroundColor: '#EA8282',
+            color: 'white',
+            borderColor: '#EA8282'
+          }
+        } else if (option === correctAnswer) {
+          return {
+            backgroundColor: '#60BF88',
+            color: 'white',
+            borderColor: '#60BF88'
+          }
+        } else {
+          return {
+            backgroundColor: 'white',
+            color: '#6066D0',
+            borderColor: '#6066D0'
+          }
+        }
+      }
+    }
+  },
   setup() {
     const store = useStore()
-    let currentQuestionIndex = 0
-    const questions = toRaw(store.state.questions)[0]
-    const question = questions[currentQuestionIndex]?.question
-    const options = questions[currentQuestionIndex]?.options
-    const correctAnswer = questions[currentQuestionIndex]?.correctAnswer
+    let currentQuestionIndex = ref(0)
+    let questions = toRaw(store.state.questions)[0]
+    let question = computed(() => questions[currentQuestionIndex.value]?.question)
+    let options = computed(() => questions[currentQuestionIndex.value]?.options)
+    let correctAnswer = computed(() => questions[currentQuestionIndex.value]?.correctAnswer)
     const selectedOption = reactive({
       value: '',
       points: 0,
       showNextButton: false,
-      result: false
+      result: false,
+      showAnswer: false
     })
+
+    function nextQuestion() {
+      currentQuestionIndex.value++
+      if (currentQuestionIndex.value < questions.length) {
+        selectedOption.value = ''
+        selectedOption.showNextButton = false
+        selectedOption.result = false
+        selectedOption.showAnswer = false
+      }
+    }
 
     function handleAnswer(value) {
       selectedOption.value = value
     }
+
     function checkAnswer() {
-      if (selectedOption.value === correctAnswer) {
+      if (selectedOption.value === correctAnswer.value) {
         selectedOption.points++
-        console.log('acertou')
       }
+      selectedOption.showAnswer = true
       selectedOption.showNextButton = true
       selectedOption.result = true
     }
@@ -94,7 +155,8 @@ export default {
       correctAnswer,
       handleAnswer,
       selectedOption,
-      checkAnswer
+      checkAnswer,
+      nextQuestion
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import App from './App.vue'
 import './index.css'
 import { store } from './store'
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import * as FaIcons from 'oh-vue-icons/icons/fa'
+
+const Fa = Object.values({ ...FaIcons })
+addIcons(...Fa)
+
 const app = createApp(App)
 app.use(store)
+app.component('v-icon', OhVueIcon)
 
 app.mount('#app')
